dc8f52c27b0da849369b404b283ed500f13c4465
kamil-ismael <hei.ismael.3@gmail.com>
2026-09-18 13:36:33 +0300

    ajout du boucle pour parcourir le tableau d'entier qui permet de savoir si le tableau d'entier contient des nombre negatif, nul, ou positif

--- Exercice1.js
+++ Exercice1.js
@@ -1,6 +1,16 @@
-function NegatifPositif(tableau) {
+function NegatifPositif(tableauEntier) {
 	const negatifsOuNuls = [];
 	const positifs = [];
 	let nombreNegatifsOuNuls = 0;
 	let nombrePositifs = 0;
+
+    for (let i = 0; i < tableauEntier.length; i++) {
+		if (tableauEntier[i] <= 0) {
+			negatifsOuNuls[nombreNegatifsOuNuls] = tableauEntier[i];
+			nombreNegatifsOuNuls += 1;
+		} else {
+			positifs[nombrePositifs] = tableauEntier[i];
+			nombrePositifs += 1;
+		}
+	}
 }

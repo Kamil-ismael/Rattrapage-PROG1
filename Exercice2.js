6c45cd7d0aa361f0a355b2ed73ec8290bea188e8
kamil-ismael <hei.ismael.3@gmail.com>
2026-09-18 14:32:44 +0300

    feate: add function Ranger

--- Exercice2.js
+++ Exercice2.js
@@ -5,8 +5,27 @@ function Echanger(tableau, premierIndex, deuxiemeIndex) {
 
     return tableau;
 }
+console.log(Echanger([0, 1, 0], 1, 2));
 
 function Ranger(tableau) {
 	let indexGauche = 0;
 	let indexDroit = tableau.length - 1;
+
+	while (indexGauche < indexDroit) {
+		while (indexGauche < indexDroit && tableau[indexGauche] === 0) {
+			indexGauche += 1;
+		}
+
+		while (indexGauche < indexDroit && tableau[indexDroit] === 1) {
+			indexDroit -= 1;
+		}
+
+		if (indexGauche < indexDroit) {
+			Echanger(tableau, indexGauche, indexDroit);
+			indexGauche += 1;
+			indexDroit -= 1;
+		}
+	}
+	return tableau;
 }
+console.log(Ranger([0, 1, 0, 0, 1, 0]))
